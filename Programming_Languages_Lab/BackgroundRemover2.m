bd024b051f6f1d3c1d2554b6efaac9d866f47737
pakoti <lovesong7zip@gmail.com>
2023-10-30 13:00:14 +0330

    photo,bckrm added

--- Programming_Languages_Lab/BackgroundRemover2.m
+++ Programming_Languages_Lab/BackgroundRemover2.m
@@ -1,3 +1,4 @@
+%enter image directory in here
 S='';
 I=imread(S);
 
@@ -5,15 +6,24 @@ Figure(1);
 imshow(I);
 figure(2);
 
+
+%enter image colors ranges here
 idx1=((I(:,:,1) > 107 & I(:,:,1) > 209));
 idx2=((I(:,:,2) > 107 & I(:,:,1) > 209));
 idx3=((I(:,:,3) > 107 & I(:,:,1) > 209));
 
 IDX=idx1 .* idx2 .* idx3 ;
 
+IDX=logical(IDX);
+
 sum(sum(sum(IDX)));
 
 I2=I;
+size(IDX)
+IDX=repmat(IDX,1,2,3);
+I2(IDX)=255;
+imshow(I2);
+
 
 
 
